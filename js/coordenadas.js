a3b063421111947f5310f09c43e4395a8c8299eb
CarlosTav91 <carlosalbertotavera91@gmail.com>
2024-10-24 22:32:04 -0500

    Add files via upload

---
+++ js/coordenadas.js
@@ -0,0 +1,616 @@
+puntosInteres = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121749,
+					7.137439
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121655,
+					7.137457
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121554,
+					7.137476
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121473,
+					7.137492
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121377,
+					7.137508
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121285,
+					7.137534
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121197,
+					7.137556
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.1211,
+					7.137633
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.12109,
+					7.13758
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121079,
+					7.13751
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121712,
+					7.137292
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121669,
+					7.137133
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121658,
+					7.137055
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121647,
+					7.137013
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121634,
+					7.136946
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121521,
+					7.13701
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121444,
+					7.137034
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121374,
+					7.137024
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121299,
+					7.137074
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121232,
+					7.13709
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121162,
+					7.137119
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.121082,
+					7.137135
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.119377,
+					7.135738
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.119384,
+					7.135489
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.119258,
+					7.135505
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.119294,
+					7.135735
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.119196,
+					7.135496
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.119212,
+					7.135743
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.11911,
+					7.13573
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.119043,
+					7.135482
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.119038,
+					7.135671
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.118965,
+					7.13549
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.118944,
+					7.13569
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.118815,
+					7.135669
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.11881,
+					7.135477
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.123933,
+					7.140573
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.12393,
+					7.140403
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.123791,
+					7.140576
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.12353,
+					7.140621
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.123479,
+					7.14051
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.123868,
+					7.14019
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.123844,
+					7.139932
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.123707,
+					7.140254
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.123651,
+					7.139988
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.123485,
+					7.140161
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.123407,
+					7.140318
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.123324,
+					7.140036
+				]
+			}
+		}
+	]
+}
---
+++ js/datos.js
@@ -0,0 +1,74 @@
+vectoresGeojson = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-73.119298,
+							7.14014
+						],
+						[
+							-73.119298,
+							7.141497
+						],
+						[
+							-73.11755,
+							7.141497
+						],
+						[
+							-73.11755,
+							7.14014
+						],
+						[
+							-73.119298,
+							7.14014
+						]
+					]
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-73.120854,
+					7.140369
+				]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"autor": "Richard Flores"
+			},
+			"geometry": {
+				"type": "LineString",
+				"coordinates": [
+					[
+						-73.122356,
+						7.140491
+					],
+					[
+						-73.120296,
+						7.140481
+					],
+					[
+						-73.120339,
+						7.140885
+					]
+				]
+			}
+		}
+	]
+}
